aaf2f6581b6b904401a81eedc11a24da79609677
Codiphile <mudits798@gmail.com>
2024-12-29 19:33:40 +0530

    Footer Completed Successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import About from "./sections/About";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
@@ -14,6 +15,7 @@ const App = () => {
       <Projects />
       <Clients />
       <Contact />
+      <Footer />
     </main>
   );
 };
