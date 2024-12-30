4e0d5d3e0f2a9cb5f7761e805fa49a5538262ed1
Codiphile <mudits798@gmail.com>
2024-12-30 18:12:37 +0530

    Contact button Fixed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -79,7 +79,14 @@ const About = () => {
               <p className="grid-subtext">
                 I'm based in Chennai/Jaipur, with remote work available.
               </p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+
+              <a href="#contact">
+                <Button
+                  name="Contact Me"
+                  isBeam
+                  containerClass="w-full mt-10"
+                />
+              </a>
             </div>
           </div>
         </div>
